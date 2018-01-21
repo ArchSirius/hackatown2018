60999ea028388bdd5bf82c4e9e1418116568df6e
Samuel Rondeau <samuel.rondeau@polymtl.ca>
2018-01-20 22:05:21 -0500

    Mock more data

--- Backend/seed.js
+++ Backend/seed.js
@@ -15,133 +15,133 @@ module.exports = function() {
 		})
 		// Seed users
 		.then(() => User.create([{
-			username: 'Alice Carroll',
-			email: 'alice@example.com',
-      password: 'foobar',
-      points: 30,
-      skills: [{
-				name: 'animal care',
-				value: '1'
+			"username": "Alice Carroll",
+			"email": "alice@example.com",
+			"password": "foobar",
+			"points": 30,
+			"skills": [{
+				"name": "animal care",
+				"value": "1"
 			}, {
-				name: 'driving',
-				value: '3'
+				"name": "driving",
+				"value": "3"
 			}, {
-				name: 'painting',
-				value: '3'
+				"name": "painting",
+				"value": "3"
 			}]
 		}, {
-			username: 'Bob Gratton',
-			email: 'bob@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'animal care',
-				value: '1'
+			"username": "Bob Gratton",
+			"email": "bob@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "animal care",
+				"value": "1"
 			},{
-				name: 'strong',
-				value: '0'
+				"name": "strong",
+				"value": "0"
 			},{
-				name: 'plumbing',
-				value: '5'
+				"name": "plumbing",
+				"value": "5"
 			},{
-				name: 'driving',
-				value: '2'
+				"name": "driving",
+				"value": "2"
 			}]
 		}, {
-			username: 'Linda Fitzgerald',
-			email: 'linda@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'driving',
-				value: '6'
+			"username": "Linda Fitzgerald",
+			"email": "linda@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "driving",
+				"value": "6"
 			},{
-				name: 'carpentry',
-				value: '2'
+				"name": "carpentry",
+				"value": "2"
 			}]
 		}, {
-			username: 'Carole	Mccoy',
-			email: 'carole@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'painting',
-				value: '3'
+			"username": "Carole	Mccoy",
+			"email": "carole@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "painting",
+				"value": "3"
 			}]
 		}, {
-			username: 'Gary Jones',
-			email: 'gary@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'cooking',
-				value: '3'
+			"username": "Gary Jones",
+			"email": "gary@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "cooking",
+				"value": "3"
 			}, {
-				name: 'driving',
-				value: '3'
+				"name": "driving",
+				"value": "3"
 			}]
 		}, {
-			username: 'Bethany Baldwin',
-			email: 'beth@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'plumbing',
-				value: '2'
+			"username": "Bethany Baldwin",
+			"email": "beth@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "plumbing",
+				"value": "2"
 			},{
-				name: 'driving',
-				value: '4'
+				"name": "driving",
+				"value": "4"
 			},{
-				name: 'strong',
-				value: '4'
+				"name": "strong",
+				"value": "4"
 			}]
 		}, {
-			username: 'Kirk	Yates',
-			email: 'kirk@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'animal care',
-				value: '1'
+			"username": "Kirk Yates",
+			"email": "kirk@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "animal care",
+				"value": "1"
 			}, {
-				name: 'writing',
-				value: '2'
+				"name": "writing",
+				"value": "2"
 			}]
 		}, {
-			username: 'Manuel Murphy',
-			email: 'manuel@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'driving',
-				value: '2'
+			"username": "Manuel Murphy",
+			"email": "manuel@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "driving",
+				"value": "2"
 			},{
-				name: 'cleaning',
-				value: '3'
+				"name": "cleaning",
+				"value": "3"
 			}]
 		}, {
-			username: 'Naomi Harris',
-			email: 'naomi@example.com',
-			password: 'foobaz',
-			points: 50,
-			skills: [{
-				name: 'painting',
-				value: '3'
+			"username": "Naomi Harris",
+			"email": "naomi@example.com",
+			"password": "foobar",
+			"points": 50,
+			"skills": [{
+				"name": "painting",
+				"value": "3"
 			}]
 		}, {
-      username: 'Patrick Williams',
-			email: 'patrick@example.com',
-			password: 'foobarbaz',
-			points: 25,
-			skills: [{
-				name: 'cleaning',
-				value: '4'
+			"username": "Patrick Williams",
+			"email": "patrick@example.com",
+			"password": "foobarbaz",
+			"points": 25,
+			"skills": [{
+				"name": "cleaning",
+				"value": "4"
 			}, {
-				name: 'driving',
-				value: '2'
+				"name": "driving",
+				"value": "2"
 			}, {
-				name: 'spellcraft',
-				value: '0'
+				"name": "spellcraft",
+				"value": "0"
 			}]
 		}]))
 		.tap(users => {
@@ -149,106 +149,94 @@ module.exports = function() {
 		})
 		// Seed tasks
 		.then(users => Task.create([{
-			name: 'Walk my dogs',
-			description: 'Fido requires daily walks, but I cannot go with him tomorrow. Help us!',
-			value: 10,
-			relevantSkills: ['animal care'],
-			creator: users[0],
-			applicants: [users[1]]
+			"name": 'Walk my dogs',
+			"description": 'Fido requires daily walks, but I cannot go with him tomorrow. Help us!',
+			"value": 10,
+			"relevantSkills": ['animal care'],
+			"creator": users[0],
+			"applicants": [users[1]]
 		}, {
-			name: 'Paint my garage',
-			description: 'My garage is my pride and joy. Help me take good care of it. I provide beer.',
-			value: 20,
-      creator: users[1],
-      relevantSkills: ['painting'],
+			"name": 'Paint my garage',
+			"description": 'My garage is my pride and joy. Help me take good care of it. I provide beer.',
+			"value": 20,
+			"creator": users[1],
+			"relevantSkills": ['painting'],
 		}, {
-      name: 'Shovel my parking',
-      description: 'I need this done by next Monday.',
-			value: 10,
-      creator: users[2],
-      relevantSkills: ['strong'],
-      applicants: [users[4], users[3]],
+			"name": 'Shovel my parking',
+			"description": 'I need this "done" by next Monday.',
+			"value": 10,
+			"creator": users[2],
+			"relevantSkills": ['strong'],
+			"applicants": [users[4], users[3]],
 		}, {
-      name: 'Make me a sandwich',
-      description: 'I need a sandwich.',
-			value: 5,
-      creator: users[2],
-      relevantSkills: ['cooking'],
-      applicants: [users[4], users[5], users[6]],
-      chosen: users[5],
-      done: true
+			"name": 'Make me a sandwich',
+			"description": 'I need a sandwich.',
+			"value": 5,
+			"creator": users[2],
+			"relevantSkills": ['cooking'],
+			"applicants": [users[4], users[5], users[6]],
+			"chosen": users[5]
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'Sewing lessons',
+			"description": 'I would like to learn how to sew. Can you teach me?',
+			"value": 15,
+			"creator": users[9],
+			"relevantSkills": ['sewing', 'patience'],
+			"applicants": [users[2], users[1]],
+			"chosen": users[1],
+			"done": true
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'Show me how to whistle',
+			"description": 'I\'ve been trying for a long time and I need an expert :(',
+			"value": 5,
+			"creator": users[1],
+			"applicants": [users[9], users[7]],
+			"chosen": users[9],
+			"done": true
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'Moving furniture',
+			"description": 'I\'m moving out and my back is killing me. I need your muscles.',
+			"value": 25,
+			"creator": users[0],
+			"relevantSkills": ['strong'],
+			"applicants": [users[4], users[9], users[6]],
+			"chosen": users[1]
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'Jog with me',
+			"description": 'Jogging is hard to do alone. I need company.',
+			"value": 01,
+			"creator": users[0],
+			"relevantSkills": ['physical'],
+			"applicants": [users[1], users[9]],
+			"chosen": users[1],
+			"done": true
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'Park cleaning',
+			"description": 'Join us to clean local parks :)',
+			"value": 40,
+			"creator": users[5],
+			"chosen": users[1]
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'TV setup',
+			"description": 'I bought a new TV but I would like help for the setup.',
+			"value": 5,
+			"creator": users[4],
+			"relevantSkills": ['geek', 'electronics']
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'Pool cleaning',
+			"description": 'Come help me clean my pool and enjoy the water!',
+			"value": 0,
+			"creator": users[8],
+			"applicants": [users[1]],
+			"chosen": users[1],
+			"done": true
 		}, {
-      name: '',
-      description: '',
-			value: 0,
-      creator: users[0],
-      relevantSkills: [],
-      applicants: [],
-      chosen: users[1],
-      done: true
+			"name": 'Financial advice',
+			"description": 'I am a total noob in financial stuff and need some help with my taxes.',
+			"value": 30,
+			"creator": users[9],
+			"relevantSkills": ['finance'],
+			"applicants": [users[0], users[7]]
 		}]))
 		.then(tasks => {
 			console.log(`${tasks.length} tasks created`);
