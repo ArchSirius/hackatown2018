35e28f8546c7c440503d9c6bc0d79cbb253846aa
Francis <francis.rochon99@gmail.com>
2018-01-20 23:24:25 -0500

    get redux state in containers

--- Frontend/src/pages/AppliedRequests/index.js
+++ Frontend/src/pages/AppliedRequests/index.js
@@ -1,65 +1,9 @@
 import { connect } from 'react-redux';
 import AppliedRequests from './AppliedRequests';
-
-const tasks = [
-  {
-    id: 1,
-    name: 'Shovel my parking',
-    category: 'Category',
-    description: 'I need someone strong to shovel my parking',
-    carepoints: 100,
-    creator: 1,
-    applicants: [
-      { id: 1, name: 'Bob Gratton' },
-      { id: 2, name: 'Bob Gratton' },
-    ],
-    chosen: undefined,
-    done: false,
-  },
-  {
-    id: 2,
-    name: 'Shovel my parking',
-    category: 'Category',
-    description: 'I need someone strong to shovel my parking',
-    carepoints: 100,
-    creator: 1,
-    applicants: [
-      { id: 1, name: 'Bob Gratton' },
-      { id: 2, name: 'Bob Gratton' },
-      { id: 3, name: 'Bob Gratton' },
-    ],
-    chosen: undefined,
-    done: false,
-  },
-  {
-    id: 3,
-    name: 'Shovel my parking',
-    category: 'Category',
-    description: 'I need someone strong to shovel my parking',
-    carepoints: 100,
-    creator: 1,
-    applicants: [],
-    chosen: undefined,
-    done: false,
-  },
-  {
-    id: 4,
-    name: 'Shovel my parking',
-    category: 'Category',
-    description: 'I need someone strong to shovel my parking',
-    carepoints: 100,
-    creator: 3,
-    applicants: [
-      { id: 1, name: 'Bob Gratton' },
-      { id: 2, name: 'Bob Gratton' },
-    ],
-    chosen: 2,
-    done: false,
-  },
-];
+import { taskOperations, taskSelectors } from '../../redux/modules/task';
 
 const mapStateToProps = state => ({
-  tasks,
+  tasks: taskSelectors.getAppliedTasks(state),
 });
 const mapDispatchToProps = dispatch => ({});
 
